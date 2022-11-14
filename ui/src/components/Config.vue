c8abb6dfc6bbb19a63800d69089e58fe79c9c832
Minjun Kwak <mk496@duke.edu>
2022-11-14 12:14:37 -0500

    Done with config page ui

--- ui/src/components/Config.vue
+++ ui/src/components/Config.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="mx-3 my-3">
-    <b-jumbotron header="Config Editor" />
-    <b-container fluid class="my-4">
-      <b-row>
+  <div>
+    <b-jumbotron class="text-center" header="Config Editor">
+      <hr class="my-4">
+      <h4 class="font-weight-normal">Omega Kickers</h4>
+    </b-jumbotron>
+    <b-container class="my-4">
+      <b-row align-h="center">
         <b-col xs="12" sm="9">
           <b-overlay :show="busy" :opacity="0.85" :blur="'2px'" rounded="sm">
             <b-form>
-              <b-form-group label="Number of Decks:" label-for="num-decks-input">
-                <b-form-input id="num-decks-input" v-model="currentConfig.numberOfDecks" placeholder="2" number />
+              <b-form-group label="Ball Speed:" label-for="ball-speed-input">
+                <b-form-input id="ball-speed-input" v-model="currentConfig.ballSpeed" placeholder="5" number />
               </b-form-group>
-              <b-form-group label="Rank Limit:" label-for="rank-limit-input">
-                <b-form-input id="rank-limit-input" v-model="currentConfig.rankLimit" placeholder="2" number />
+              <b-form-invalid-feedback class="mb-2" :state="!ballSpeedError">{{ errorMsg }}</b-form-invalid-feedback>
+              <b-form-group label="Player Speed:" label-for="player-speed-input">
+                <b-form-input id="player-speed-input" v-model="currentConfig.playerSpeed" placeholder="5" number />
               </b-form-group>
-              <b-form-invalid-feedback :state="!errorMsg">{{ "Invalid config parameters" }}</b-form-invalid-feedback>
-              <b-button class="mt-2 mb-2" @click="saveConfig()">
-                Save
-              </b-button>
+              <b-form-invalid-feedback class="mb-2" :state="!playerSpeedError">{{ errorMsg }}</b-form-invalid-feedback>
+              <b-form-group label="Obstacles:">
+                <ConfigButtonGroup :toggle="currentConfig.obstacles" :switch-toggle="switchObstacleToggle" />
+              </b-form-group>
+              <b-form-group label="Goalkeepers:">
+                <ConfigButtonGroup :toggle="currentConfig.keepers" :switch-toggle="switchKeeperToggle" />
+              </b-form-group>
+              <b-row align-h="start">
+                <b-col cols="2">
+                  <b-button class="mt-2 mb-2 w-100" @click="saveConfig()">
+                    Save
+                  </b-button>
+                </b-col>
+                <b-col cols="2">
+                  <b-alert :show="dismissAlert" variant="light" @dismissed="dismissAlert = 0"
+                    @dismiss-count-down="countDownChanged">
+                    <b-icon font-scale="2" :icon="errorIcon ? 'x-square' : 'check-square'"
+                      :variant="errorIcon ? 'danger' : 'success'" />
+                  </b-alert>
+                </b-col>
+                <b-col cols="6" />
+              </b-row>
             </b-form>
           </b-overlay>
         </b-col>
@@ -25,12 +47,18 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { io } from "socket.io-client"
+import ConfigButtonGroup from './ConfigButtonGroup.vue';
 
-let currentConfig = ref({ numberOfDecks: null, rankLimit: null })
-let errorMsg = ref(false)
+let currentConfig = ref({ ballSpeed: 0, playerSpeed: 0, obstacles: false, keepers: false })
+let ballSpeedError = ref(false)
+let playerSpeedError = ref(false)
+const errorMsg = "Enter a number!"
 let busy = ref(false)
+let errorIcon = ref(false)
+let dismissAlert = ref(0)
+let dismissSecs = ref(3)
 const socket = io()
 
 onMounted(() => {
@@ -42,12 +70,27 @@ onMounted(() => {
   })
 })
 
+function switchObstacleToggle() {
+  currentConfig.value.obstacles = !currentConfig.value.obstacles
+}
+
+function switchKeeperToggle() {
+  currentConfig.value.keepers = !currentConfig.value.keepers
+}
+
+function countDownChanged(dismissCountDown: number) {
+  dismissAlert.value = dismissCountDown
+}
+
 function saveConfig() {
   busy.value = true
   socket.emit('update-config', currentConfig.value)
   socket.on('update-config-reply', (result) => {
-    !result ? errorMsg.value = true : errorMsg.value = false
+    result.ballError ? ballSpeedError.value = true : ballSpeedError.value = false
+    result.playerError ? playerSpeedError.value = true : playerSpeedError.value = false
     busy.value = false
+    result.updated ? errorIcon.value = false : errorIcon.value = true
+    dismissAlert.value = dismissSecs.value
   })
 }
 </script>
---
+++ ui/src/components/ConfigButtonGroup.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-button-group>
+    <b-button :variant="toggle ? 'dark' : 'light'" @click="switchToggle">On</b-button>
+    <b-button :variant="toggle ? 'light' : 'dark'" @click="switchToggle">Off</b-button>
+  </b-button-group>
+</template>
+<script setup lang="ts">
+// props
+interface Props {
+  toggle?: boolean,
+  switchToggle?: Function
+}
+
+// default values for props
+const props = withDefaults(defineProps<Props>(), {
+  toggle: false,
+})
+
+</script>
